d972792f1ebbefe4259aa0da4077cb46b1b39863
Pedro-Souza <programadorhacker8@gmail.com>
2017-09-25 23:16:54 -0300

    removendo bibliotecas para req

--- src/cli.js
+++ src/cli.js
@@ -1,19 +1,4 @@
 #!/usr/bin/env node
 
-//import { green, red } from "colors/safe";
-//import { request } from "request";
-//import getCoins from "./coins";
-//import { http } from 'http';
-
-import { http } from 'http';
-
-
-let request = require('request');
-
 const emitSuccess = message => console.log(green("✔ Passou"));
 const emitError = message => console.log(red("✗ Error"));
-
-
-request.get('httops://google.com', (err, response, body) => {
-    console.log(err);
-})
