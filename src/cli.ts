f13335a9ab9559031337da3241596e458c673d14
Pedro-Souza <programadorhacker8@gmail.com>
2017-10-10 21:33:34 -0300

    remove files .js and add files .ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
 
+import { green, red } from "colors/safe";
+
 const emitSuccess = message => console.log(green("✔ Passou"));
 const emitError = message => console.log(red("✗ Error"));
+
+emitSuccess("ha");
