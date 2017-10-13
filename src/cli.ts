30225b6caf647bd36a23a6cf61e98ac1cc7197a5
Pedro-Souza <programadorhacker8@gmail.com>
2017-10-12 22:07:19 -0300

    adicionando cli e tsconfig

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,32 @@
 #!/usr/bin/env node
-
+import * as yargs from "yargs";
 import { green, red } from "colors/safe";
+import { getPolo } from './coins';
 
-const emitSuccess = message => console.log(green("✔ Passou"));
-const emitError = message => console.log(red("✗ Error"));
+export const emitSuccess = message => console.log(green(`${message} ✔ Passou`));
+export const emitError = message => console.log(red(`${message} Error`));
 
-emitSuccess("ha");
+
+export class cli{
+    constructor() {
+        console.log("Me construiu");
+        this.cli(
+            yargs.usage('Uso $0 --coin <coin>')
+                 .option('coin', {
+                     describe: 'Informe a criptomoeda desejada!',
+                     type: 'string'
+                  }).option('teste', {
+                      describe: 'Mais um teste',
+                      type: 'string'
+                  })
+                  .demandOption(['coin']).locale('pt_BR').strict().help().version().argv
+                )
+    }
+    cli(args) {
+        if(args.coin) {
+            console.log("aqui => " + args.coin);
+            const tt = new getPolo();
+        }
+    }
+}
+const lul = new cli();
