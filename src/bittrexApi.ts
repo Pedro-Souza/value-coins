6cc1a3d12705a3f41140820977504e844596ead6
joja <jjcardonap@gmail.com>
2017-10-22 19:55:57 -0600

    added bittrex api ticker call and exchange option

---
+++ src/bittrexApi.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { green } from "colors/safe";
+
+export class BittrexApi{
+    
+    baseUrl: string = "https://bittrex.com/api/v1.1/";
+
+    getTicker(coin){
+        let url: string = this.baseUrl + "public/getmarketsummary?market=btc-" +coin.toUpperCase();
+        axios.get(url).then(response => {
+            if(response.data.success!=true) 
+                throw new Error('Error on consulting Bittrex api url (' + url + ').\n' + response.data.message);
+            
+            var result = response.data.result[0];
+            console.log(green("Valor => " + result.Last));
+            console.log(green("Menor valor => " + result.Low));
+            console.log(green("Volume => " + result.Volume));
+        }).catch(err => {
+            console.log(err);
+        });
+    }
+
+}
--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,7 @@
 import * as yargs from "yargs";
 import { green, red } from "colors/safe";
 import { getPolo } from './coins';
+import { BittrexApi } from './bittrexApi';
 
 //export const emitSuccess = message => console.log(green(`${message} ✔ Passou`));
 //export const emitError = message => console.log(red(`${message} Error`));
@@ -11,12 +12,18 @@ import { getPolo } from './coins';
 export class cli{
     constructor() {
         this.cli(
-            yargs.usage('Uso $0 --coin <coin>')
-                 .option('coin', {
-                     describe: 'Informe a criptomoeda desejada!',
-                     type: 'string'
-                  })
-                  .demandOption(['coin'])
+            yargs.usage('Uso $0 --coin <coin> --exchange <exchange>')
+                 .options({
+                    'coin': {
+                        describe: 'Informe a criptomoeda desejada!',
+                        type: 'string'
+                     },
+                     'exchange': {
+                         describe: 'Informe a exchange desejada! (poloniex ou bittrex)',
+                         type: 'string'
+                     }
+                 })
+                  .demandOption(['coin','exchange'])
                   .locale('pt_BR')
                   .strict()
                   .help()
@@ -25,9 +32,20 @@ export class cli{
                 )
     }
     cli(args) {
-        if(args.coin) {
-            const coin = new getPolo();
-            coin.getCoin(args.coin.toUpperCase());
+        if(args.coin && args.exchange) {
+            //Getting ticker from polo
+            if(args.exchange.toLowerCase() === 'polo' || args.exchange.toLowerCase() === 'poloniex'){
+                const coinPolo = new getPolo();
+                coinPolo.getCoin(args.coin.toUpperCase());
+            }
+            //Getting ticker from bittrex
+            else if(args.exchange.toLowerCase() === 'bittrex'){
+                let bittrexApi = new BittrexApi();
+                bittrexApi.getTicker(args.coin);
+            }
+            else{
+                console.log(red('Exchange not supported yet.'));
+            }
         }
     }
 }
